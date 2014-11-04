58e165e7189e78d36362ee83a9eeabc761242017
Evan Gray <evan.gray@rackspace.com>
2014-11-04 16:03:18 -0600

    100% less snark, and 95% less stack trace

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ require 'json'
 
 require './lib/is_pwnd.rb'
 
+configure do
+  # Don't log them. We'll do that ourself
+  set :dump_errors, false
+
+  # Don't capture any errors. Throw them up the stack
+  set :raise_errors, false
+
+  # Disable internal middleware for presenting errors
+  # as useful HTML pages
+  set :show_exceptions, false
+end
+
 EMAIL_LIST = JSON.parse(File.read('/tmp/config.json'))
 
 get '/' do
